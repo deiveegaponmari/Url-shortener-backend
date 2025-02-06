9965519df592280bb31209c4f8a160f651d08d64
deiveegaponmari <ponmarimdeiveega@gmail.com>
2025-02-06 17:45:44 +0530

    added

--- index.js
+++ index.js
@@ -17,21 +17,4 @@ web_server.use("/urlshort",urlShortController)
 web_server.listen(4000,()=>{
     console.log("server started successfully")
     console.log(`http://${process.env.HOST_NAME}:${process.env.HOST_PORT}`);
-})/* const express = require('express');
-const Web_Server=express();
-var cors = require('cors');
-require('dotenv').config()
-require("./dbconfig")
-const { UserRouter } = require('./controller/userRouter');
-const { urlShortController } = require('./controller/urlShortController');
-
-Web_Server.use(express.json())
-Web_Server.use(cors())
-
-//routers injection
-Web_Server.use("/user",UserRouter)
-Web_Server.use("/urlshort",urlShortController)
-Web_Server.listen(4000,"localhost",()=>{
-    console.log("server connected successfully")
-    console.log("http://localhost:4000")
-}) */
+})
